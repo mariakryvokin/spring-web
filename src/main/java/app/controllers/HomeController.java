0aecbc7a4062d21e5536b5a6917e94d44807444b
Mariia_Kryvokin <mkryvokin@corelogic.com>
2020-01-10 17:56:47 +0200

    spring security

--- src/main/java/app/controllers/HomeController.java
+++ src/main/java/app/controllers/HomeController.java
@@ -2,6 +2,7 @@ package app.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -9,5 +10,15 @@ public class HomeController {
     @GetMapping("/")
     public String home(){
         return "main";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String doLogin(){
+        return "";
     }
 }
---
+++ src/main/java/app/services/AppUserDetailsService.java
@@ -0,0 +1,31 @@
+package app.services;
+
+import app.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.getUserByEmail(email);
+        if (user != null) {
+            List<SimpleGrantedAuthority> authorities = user.getRoles().stream().map(r->new SimpleGrantedAuthority(r.getName())).collect(Collectors.toList());
+            UserDetails userDetails = new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+            return userDetails;
+        } else {
+            throw new UsernameNotFoundException("User not found.");
+        }
+    }
+}
