be3afc473096650f6d93ac5bb62059f8d160b469
Mariia_Kryvokin <mkryvokin@corelogic.com>
2020-01-24 17:02:54 +0200

    transactional activated

--- src/main/java/app/config/RepositoryConfig.java
+++ src/main/java/app/config/RepositoryConfig.java
@@ -13,12 +13,14 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 
 @Configuration
 @EnableJpaRepositories(basePackages="app.repositories")
+@EnableTransactionManagement
 @PropertySource("classpath:application.properties")
 public class RepositoryConfig {
 
